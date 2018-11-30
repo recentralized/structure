9895fdc09211966eb77ba882e7c81fcdf670efd2
Ryan Carver <ryan@ryancarver.com>
2018-11-30 13:27:33 -0800

    example of metadata

---
+++ examples/meta/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/recentralized/structure/content"
+)
+
+func main() {
+	meta, err := buildMeta()
+
+	if err != nil {
+		fmt.Printf("Failed to build meta: %s", err)
+		os.Exit(1)
+	}
+
+	data, err := json.MarshalIndent(meta, "", "  ")
+	if err != nil {
+		fmt.Printf("Failed to create json: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(data))
+}
+
+func buildMeta() (*content.Meta, error) {
+
+	meta := &content.Meta{
+		ContentType: content.JPG,
+		Size:        1024,
+	}
+
+	meta.Inherent = content.MetaContent{
+		Created: time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+		Image: content.MetaImage{
+			Width:  3000,
+			Height: 5000,
+		},
+		Exif: content.Exif{
+			"ExposureTime": content.ExifValue{ID: "ShutterSpeed", Val: "1/60"},
+		},
+	}
+	meta.Sidecar = content.MetaContent{
+		Exif: content.Exif{
+			"FNumber": content.ExifValue{ID: "0x829d", Val: 1.8},
+		},
+	}
+
+	return meta, nil
+}
