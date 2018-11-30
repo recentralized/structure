53b7ba917a92671f4525e37d303191ffe9defc62
Ryan Carver <ryan@ryancarver.com>
2018-11-29 20:53:13 -0800

    updaet json test style

--- v0/dstjson_test.go
+++ v0/dstjson_test.go
@@ -12,17 +12,17 @@ import (
 
 func TestDstJSON(t *testing.T) {
 	tests := []struct {
+		desc string
 		dst  Dst
 		json string
 	}{
-
-		// Zero value.
 		{
+			desc: "zero value",
 			dst:  Dst{},
 			json: `{"dst_id":"","index_uri":"","data_uri":"","meta_uri":""}`,
 		},
-		// Basic fields.
 		{
+			desc: "basic fields",
 			dst: Dst{
 				DstID:    DstID("abc"),
 				IndexURI: uri.TrustedNew("http://example.com/"),
@@ -32,41 +32,40 @@ func TestDstJSON(t *testing.T) {
 			json: `{"dst_id":"abc","index_uri":"http://example.com/","data_uri":"http://example.com/data","meta_uri":"http://example.com/meta"}`,
 		},
 	}
-	for i, test := range tests {
-		data, err := json.Marshal(test.dst)
+	for _, tt := range tests {
+		data, err := json.Marshal(tt.dst)
 		if err != nil {
-			t.Fatalf("%d Dst failed to marshal: %s", i, err)
+			t.Fatalf("%q Dst failed to marshal: %s", tt.desc, err)
 		}
-		if test.json != "" {
-			if string(data) != test.json {
-				t.Errorf("%d Dst JSON\ngot  %s\nwant %s", i, data, test.json)
-			}
+		if string(data) != tt.json {
+			t.Errorf("%q Dst JSON\ngot  %s\nwant %s", tt.desc, data, tt.json)
 		}
-		log.Printf("%d Dst JSON %s", i, data)
+		log.Printf("%q Dst JSON %s", tt.desc, data)
 		got := Dst{}
 		err = json.Unmarshal(data, &got)
 		if err != nil {
-			t.Fatalf("%d Dst failed to unmarshal: %s", i, err)
+			t.Fatalf("%q Dst failed to unmarshal: %s", tt.desc, err)
 		}
-		if !reflect.DeepEqual(test.dst, got) {
-			t.Errorf("%d Dst Roundtrip\ngot  %#v\nwant %#v", i, got, test.dst)
+		if !reflect.DeepEqual(tt.dst, got) {
+			t.Errorf("%q Dst Roundtrip\ngot  %#v\nwant %#v", tt.desc, got, tt.dst)
 		}
 	}
 }
 
 func TestDstItemJSON(t *testing.T) {
 	tests := []struct {
+		desc string
 		item DstItem
 		json string
 	}{
 
-		// Zero value.
 		{
+			desc: "zero value",
 			item: DstItem{},
 			json: `{"dst_id":"","data_uri":"","meta_uri":"","stored_at":null}`,
 		},
-		// Basic fields.
 		{
+			desc: "basic fields",
 			item: DstItem{
 				DstID:    DstID("abc"),
 				DataURI:  uri.TrustedNew("http://example.com/data/abc.jpg"),
@@ -76,24 +75,22 @@ func TestDstItemJSON(t *testing.T) {
 			json: `{"dst_id":"abc","data_uri":"http://example.com/data/abc.jpg","meta_uri":"http://example.com/meta/abc.json","stored_at":"0001-02-03T04:05:06.000000007Z"}`,
 		},
 	}
-	for i, test := range tests {
-		data, err := json.Marshal(test.item)
+	for _, tt := range tests {
+		data, err := json.Marshal(tt.item)
 		if err != nil {
-			t.Fatalf("%d DstItem failed to marshal: %s", i, err)
+			t.Fatalf("%q DstItem failed to marshal: %s", tt.desc, err)
 		}
-		if test.json != "" {
-			if string(data) != test.json {
-				t.Errorf("%d DstItem JSON\ngot  %s\nwant %s", i, data, test.json)
-			}
+		if string(data) != tt.json {
+			t.Errorf("%q DstItem JSON\ngot  %s\nwant %s", tt.desc, data, tt.json)
 		}
-		log.Printf("%d DstItem JSON %s", i, data)
+		log.Printf("%q DstItem JSON %s", tt.desc, data)
 		got := DstItem{}
 		err = json.Unmarshal(data, &got)
 		if err != nil {
-			t.Fatalf("%d DstItem failed to unmarshal: %s", i, err)
+			t.Fatalf("%q DstItem failed to unmarshal: %s", tt.desc, err)
 		}
-		if !reflect.DeepEqual(test.item, got) {
-			t.Errorf("%d DstItem Roundtrip\ngot  %#v\nwant %#v", i, got, test.item)
+		if !reflect.DeepEqual(tt.item, got) {
+			t.Errorf("%q DstItem Roundtrip\ngot  %#v\nwant %#v", tt.desc, got, tt.item)
 		}
 	}
 }
