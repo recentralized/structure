971c527cad9164dab26d984478b981e36361a399
Ryan Carver <ryan@ryancarver.com>
2018-11-30 09:24:09 -0800

    functioning example of an index

---
+++ examples/index/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/recentralized/structure/index"
+	"github.com/recentralized/structure/uri"
+)
+
+func main() {
+	index, err := buildIndex()
+	if err != nil {
+		fmt.Printf("Failed to build index: %s", err)
+		os.Exit(1)
+	}
+
+	data, err := json.MarshalIndent(index, "", "  ")
+	if err != nil {
+		fmt.Printf("Failed to create json: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(data))
+}
+
+func buildIndex() (*index.Index, error) {
+
+	srcPath, err := uri.NewDirPath("/Users/rcarver/Pictures/samples")
+	if err != nil {
+		return nil, fmt.Errorf("Could not create src path: %s", err)
+	}
+
+	dstPath, err := uri.NewDirPath("/Users/rcarver/Pictures/recentralized")
+	if err != nil {
+		return nil, fmt.Errorf("Could not create dst path: %s", err)
+	}
+
+	src := index.NewSrc(srcPath.URI)
+	dst := index.NewDstWithStandardPaths(dstPath.URI)
+
+	idx := &index.Index{
+		Srcs: []index.Src{src},
+		Dsts: []index.Dst{dst},
+	}
+
+	return idx, nil
+}
