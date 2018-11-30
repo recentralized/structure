9fcbdedcf79edf034ea6c868f69b9bd244dca4c3
Ryan Carver <ryan@ryancarver.com>
2018-11-30 15:36:06 -0800

    Index.AddRef

--- index/index.go
+++ index/index.go
@@ -6,3 +6,24 @@ type Index struct {
 	Dsts []Dst   `json:"dsts,omitempty"`
 	Refs []*URef `json:"refs,omitempty"`
 }
+
+// AddRef implements the logic to add a Ref to the Index. It's idempotent; if
+// either the Ref's Src or Dst was actually added, the method returns true.
+// Storage implementations that write directly to a database should probably
+// implement this logic directly in the db.
+func (i *Index) AddRef(ref Ref) bool {
+	var uref *URef
+	for _, u := range i.Refs {
+		if u.Hash.EqualHash(ref.Hash) {
+			uref = u
+			break
+		}
+	}
+	if uref == nil {
+		uref = &URef{Hash: ref.Hash}
+		i.Refs = append(i.Refs, uref)
+	}
+	addSrc := uref.AddSrc(ref.Src)
+	addDst := uref.AddDst(ref.Dst)
+	return addSrc || addDst
+}
