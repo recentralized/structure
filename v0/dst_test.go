c533e124eecb39ea619fbb90d0aaa14f6d948caf
Ryan Carver <ryan@ryancarver.com>
2018-11-29 20:56:35 -0800

    update other tests style

--- v0/dst_test.go
+++ v0/dst_test.go
@@ -9,12 +9,14 @@ import (
 
 func TestNewDst(t *testing.T) {
 	tests := []struct {
+		desc     string
 		indexURI uri.URI
 		dataURI  uri.URI
 		metaURI  uri.URI
 		want     Dst
 	}{
 		{
+			desc:     "consistent value",
 			indexURI: uri.TrustedNew("file:///tmp/"),
 			dataURI:  uri.TrustedNew("file:///tmp/data/"),
 			metaURI:  uri.TrustedNew("file:///tmp/meta/"),
@@ -26,32 +28,34 @@ func TestNewDst(t *testing.T) {
 			},
 		},
 	}
-	for i, test := range tests {
-		got := NewDst(test.indexURI, test.dataURI, test.metaURI)
-		if !reflect.DeepEqual(got, test.want) {
-			t.Errorf("%d NewDst() got %#v, want %#v", i, got, test.want)
+	for _, tt := range tests {
+		got := NewDst(tt.indexURI, tt.dataURI, tt.metaURI)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q NewDst() got %#v, want %#v", tt.desc, got, tt.want)
 		}
 	}
 }
 
 func TestNewDstIDFromURIs(t *testing.T) {
 	tests := []struct {
+		desc     string
 		indexURI uri.URI
 		dataURI  uri.URI
 		metaURI  uri.URI
 		want     DstID
 	}{
 		{
+			desc:     "conistent value",
 			indexURI: uri.TrustedNew("file:///tmp/"),
 			dataURI:  uri.TrustedNew("file:///tmp/data/"),
 			metaURI:  uri.TrustedNew("file:///tmp/meta/"),
 			want:     DstID("0b1bccaf-e8e2-5693-be3c-320031f4850a"),
 		},
 	}
-	for i, test := range tests {
-		got := newDstIDFromURIs(test.indexURI, test.dataURI, test.metaURI)
-		if got != test.want {
-			t.Errorf("%d newDstIDFromURIs got %#v, want %#v", i, got, test.want)
+	for _, tt := range tests {
+		got := newDstIDFromURIs(tt.indexURI, tt.dataURI, tt.metaURI)
+		if got != tt.want {
+			t.Errorf("%q newDstIDFromURIs got %#v, want %#v", tt.desc, got, tt.want)
 		}
 	}
 }
--- v0/src_test.go
+++ v0/src_test.go
@@ -9,10 +9,12 @@ import (
 
 func TestNewSrc(t *testing.T) {
 	tests := []struct {
+		desc   string
 		srcURI uri.URI
 		want   Src
 	}{
 		{
+			desc:   "consistent value",
 			srcURI: uri.TrustedNew("file:///tmp/"),
 			want: Src{
 				SrcID:  SrcID("901e9ee6-64b8-5d44-941c-7e22c7cb4af3"),
@@ -20,28 +22,30 @@ func TestNewSrc(t *testing.T) {
 			},
 		},
 	}
-	for i, test := range tests {
-		got := NewSrc(test.srcURI)
-		if !reflect.DeepEqual(got, test.want) {
-			t.Errorf("%d NewSrc() got %#v, want %#v", i, got, test.want)
+	for _, tt := range tests {
+		got := NewSrc(tt.srcURI)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q NewSrc() got %#v, want %#v", tt.desc, got, tt.want)
 		}
 	}
 }
 
 func TestNewSrcIDFromURI(t *testing.T) {
 	tests := []struct {
+		desc   string
 		srcURI uri.URI
 		want   SrcID
 	}{
 		{
+			desc:   "consistent value",
 			srcURI: uri.TrustedNew("file:///tmp/"),
 			want:   SrcID("901e9ee6-64b8-5d44-941c-7e22c7cb4af3"),
 		},
 	}
-	for i, test := range tests {
-		got := newSrcIDFromURI(test.srcURI)
-		if got != test.want {
-			t.Errorf("%d newSrcIDFromURIs got %#v, want %#v", i, got, test.want)
+	for _, tt := range tests {
+		got := newSrcIDFromURI(tt.srcURI)
+		if got != tt.want {
+			t.Errorf("%q newSrcIDFromURIs got %#v, want %#v", tt.desc, got, tt.want)
 		}
 	}
 }
