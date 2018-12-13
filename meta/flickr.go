40ec454ae28d40f5c9aef75ee12994e2e839a481
Ryan Carver <ryan@ryancarver.com>
2018-12-12 16:18:21 -0800

    add photo dates, change NSID to UserID

--- meta/flickr.go
+++ meta/flickr.go
@@ -3,28 +3,35 @@ package meta
 import "time"
 
 // FlickrMedia is the full set of data available for media on Flickr.
+// https://www.flickr.com/services/api/
+// https://www.flickr.com/services/api/flickr.photos.getInfo.html
 type FlickrMedia struct {
-	ID          string               `json:"id"`
-	Title       string               `json:"title,omitempty"`
-	Description string               `json:"description,omitempty"`
-	Privacy     string               `json:"privacy,omitempty"`
-	License     string               `json:"license,omitempty"`
-	URL         string               `json:"url,omitempty"`
-	Geo         *FlickrMediaGeo      `json:"geo,omitempty"`
-	Views       int                  `json:"views,omitempty"`
-	Faves       []FlickrMediaFave    `json:"faves,omitempty"`
-	Tags        []FlickrMediaTag     `json:"tags,omitempty"`
-	People      []FlickrMediaPerson  `json:"people,omitempty"`
-	Notes       []FlickrMediaNote    `json:"notes,omitempty"`
-	Sets        []FlickrMediaInSet   `json:"sets,omitempty"`
-	Pools       []FlickrMediaInPool  `json:"pools,omitempty"`
-	Comments    []FlickrMediaComment `json:"comments,omitempty"`
+	ID           string               `json:"id"`
+	UserID       string               `json:"user_id,omitempty"`
+	Username     string               `json:"username,omitempty"`
+	Title        string               `json:"title,omitempty"`
+	Description  string               `json:"description,omitempty"`
+	PostedAt     time.Time            `json:"posted_at,omitempty"`
+	TakenAt      time.Time            `json:"taken_at,omitempty"`
+	LastUpdateAt time.Time            `json:"last_update_at,omitempty"`
+	URL          string               `json:"url,omitempty"`
+	Privacy      string               `json:"privacy,omitempty"`
+	License      string               `json:"license,omitempty"`
+	Geo          *FlickrMediaGeo      `json:"geo,omitempty"`
+	Views        int                  `json:"views,omitempty"`
+	Faves        []FlickrMediaFave    `json:"faves,omitempty"`
+	Tags         []FlickrMediaTag     `json:"tags,omitempty"`
+	People       []FlickrMediaPerson  `json:"people,omitempty"`
+	Notes        []FlickrMediaNote    `json:"notes,omitempty"`
+	Sets         []FlickrMediaInSet   `json:"sets,omitempty"`
+	Pools        []FlickrMediaInPool  `json:"pools,omitempty"`
+	Comments     []FlickrMediaComment `json:"comments,omitempty"`
 }
 
 // FlickrMediaFave is who favorited an image on Flickr.
 // https://www.flickr.com/services/api/flickr.photos.getFavorites.html
 type FlickrMediaFave struct {
-	NSID     string    `json:"nsid"`
+	UserID   string    `json:"user_id"`
 	Username string    `json:"username,omitempty"`
 	Date     time.Time `json:"date,omitempty"`
 }
@@ -42,7 +49,7 @@ type FlickrMediaGeo struct {
 // https://www.flickr.com/services/api/flickr.photos.getInfo.html
 type FlickrMediaTag struct {
 	ID       string `json:"id"`
-	NSID     string `json:"nsid,omitempty"`
+	UserID   string `json:"user_id,omitempty"`
 	Username string `json:"username,omitempty"`
 	Tag      string `json:"tag,omitempty"`
 	RawTag   string `json:"raw_tag,omitempty"`
@@ -51,20 +58,20 @@ type FlickrMediaTag struct {
 // FlickrMediaPerson is a person tagged in a Flickr image.
 // https://www.flickr.com/services/api/flickr.photos.people.getList.html
 type FlickrMediaPerson struct {
-	NSID        string `json:"nsid"`
-	Username    string `json:"username,omitempty"`
-	X           int    `json:"x,omitempty"`
-	Y           int    `json:"y,omitempty"`
-	W           int    `json:"w,omitempty"`
-	H           int    `json:"h,omitempty"`
-	AddedByNSID string `json:"added_by_nsid,omitempty"`
+	UserID        string `json:"user_id"`
+	Username      string `json:"username,omitempty"`
+	X             int    `json:"x,omitempty"`
+	Y             int    `json:"y,omitempty"`
+	W             int    `json:"w,omitempty"`
+	H             int    `json:"h,omitempty"`
+	AddedByUserID string `json:"added_by_user_id,omitempty"`
 }
 
 // FlickrMediaNote is a note on a Flickr image.
 // https://www.flickr.com/services/api/flickr.photos.getInfo.html
 type FlickrMediaNote struct {
 	ID       string `json:"id"`
-	NSID     string `json:"nsid,omitempty"`
+	UserID   string `json:"user_id,omitempty"`
 	Username string `json:"username,omitempty"`
 	X        int    `json:"x,omitempty"`
 	Y        int    `json:"y,omitempty"`
@@ -93,9 +100,9 @@ type FlickrMediaInPool struct {
 // https://www.flickr.com/services/api/flickr.photos.comments.getList.html
 type FlickrMediaComment struct {
 	ID       string    `json:"id"`
-	NSID     string    `json:"nsid,omitempty"`
+	UserID   string    `json:"user_id,omitempty"`
 	Username string    `json:"username,omitempty"`
 	Text     string    `json:"text,omitempty"`
-	Date     time.Time `json:"date,omitempty"`
+	PostedAt time.Time `json:"posted_at,omitempty"`
 	URL      string    `json:"url,omitempty"`
 }
