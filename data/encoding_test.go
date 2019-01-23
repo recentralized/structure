bd55ea86575283cfc53187a48af403b089770eb8
Ryan Carver <ryan@ryancarver.com>
2019-01-23 11:09:52 -0800

    fix Hash db encoding for empty/null value

--- data/encoding_test.go
+++ data/encoding_test.go
@@ -3,6 +3,7 @@ package data
 import (
 	"encoding/json"
 	"log"
+	"reflect"
 	"testing"
 )
 
@@ -36,6 +37,30 @@ func TestHashDatabase(t *testing.T) {
 		t.Fatalf("Scan() failed: %s", err)
 	}
 	if !hashIn.Equal(*hashOut) {
-		t.Fatalf("Round-trip failed: got %s want %s", hashOut, hashIn)
+		t.Fatalf("Round-trip Equal failed: got %s want %s", hashOut, hashIn)
+	}
+	if !reflect.DeepEqual(hashIn, *hashOut) {
+		t.Fatalf("Round-trip DeepEqual failed: got %#v want %#v", hashOut, hashIn)
+	}
+}
+func TestHashDatabaseEmpty(t *testing.T) {
+	var hashIn Hash
+	val, err := hashIn.Value()
+	if err != nil {
+		t.Fatalf("Value() failed: %s", err)
+	}
+	_, ok := val.([]byte)
+	if !ok {
+		t.Fatalf("Value() did not return bytes")
+	}
+	hashOut := &Hash{}
+	if err := hashOut.Scan(val); err != nil {
+		t.Fatalf("Scan() failed: %s", err)
+	}
+	if !hashIn.Equal(*hashOut) {
+		t.Fatalf("Round-trip Equal failed: got %s want %s", hashOut, hashIn)
+	}
+	if !reflect.DeepEqual(hashIn, *hashOut) {
+		t.Fatalf("Round-trip DeepEqual failed: got %#v want %#v", hashOut, hashIn)
 	}
 }
