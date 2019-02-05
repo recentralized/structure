2c28be4269459c4749b7ce755d8f346a0f22cf4a
Ryan Carver <ryan@ryancarver.com>
2019-02-04 23:07:23 -0800

    tighten up data.Storage formatting and parsing

--- data/types.go
+++ data/types.go
@@ -24,21 +24,26 @@ const (
 
 // Encoding definitions.
 const (
+	// Native is the zero value for Encoding, meaning no encoding.
 	Native Encoding = ""
-	Tar             = "tar"
-	GZip            = "gz"
+
+	Tar  = "tar"
+	GZip = "gz"
 )
 
 // Class definitions.
 const (
-	Uncategorized Class = ""
-	Image               = "image"
+	Unclassified Class = ""
+	Image              = "image"
 )
 
 // Type is a known kind of file such as JPEG or PNG.
 type Type string
 
 func (t Type) String() string {
+	if t == "" {
+		return "data:unknown"
+	}
 	return string(t)
 }
 
@@ -52,18 +57,39 @@ func (t Type) Class() Class {
 	return classOfType[t]
 }
 
+// Ok return true if the type is defined.
+func (t Type) Ok() bool {
+	_, ok := types[t]
+	return ok
+}
+
 // ParseExt parses an extension, returning the Stored format.
 func ParseExt(str string) (Stored, error) {
+	if str == "" {
+		return Stored{}, nil
+	}
 	parts := strings.Split(str, ".")
 	if len(parts) > 2 {
 		return Stored{}, fmt.Errorf("data: too many parts in extension %q", str)
+	}
+	if len(parts) == 1 {
+		if e := Encoding(parts[0]); e.Ok() {
+			return Stored{Encoding: e}, nil
+		}
 	}
 	t := Type(parts[0])
 	e := Native
 	if len(parts) == 2 {
 		e = Encoding(parts[1])
 	}
-	return Stored{t, e}, nil
+	s := Stored{t, e}
+	if !t.Ok() {
+		return s, fmt.Errorf("data: unknown type: %q", t)
+	}
+	if !e.Ok() {
+		return s, fmt.Errorf("data: unknown encoding: %q", e)
+	}
+	return s, nil
 }
 
 // Stored is how a type is formatted for storage.
@@ -73,15 +99,45 @@ type Stored struct {
 }
 
 func (s Stored) String() string {
-	return s.Ext()
+	e := s.Ext()
+	if e == "" {
+		return "data:unknown"
+	}
+	return e
 }
 
 // Ext returns the stored data's standard file extension.
 func (s Stored) Ext() string {
-	if s.Encoding == Native {
+	switch {
+	case s.Type == UnknownType && s.Encoding == Native:
+		return ""
+	case s.Type == UnknownType:
+		return fmt.Sprintf("%s", s.Encoding.Ext())
+	case s.Encoding == Native:
 		return fmt.Sprintf("%s", s.Type.Ext())
+	default:
+		return fmt.Sprintf("%s.%s", s.Type.Ext(), s.Encoding.Ext())
 	}
-	return fmt.Sprintf("%s.%s", s.Type.Ext(), s.Encoding.Ext())
+}
+
+// Ok return true if the storage format is defined.
+func (s Stored) Ok() bool {
+	return s.Type.Ok() && s.Encoding.Ok()
+}
+
+// Format implements fmt.Formatter.
+func (s Stored) Format(f fmt.State, c rune) {
+	switch c {
+	case 's':
+		f.Write([]byte(s.String()))
+	case 'v':
+		f.Write([]byte("Stored["))
+		f.Write([]byte("type: "))
+		f.Write([]byte(s.Type.String()))
+		f.Write([]byte(", encoding: "))
+		f.Write([]byte(s.Encoding.String()))
+		f.Write([]byte("]"))
+	}
 }
 
 // Encoding is the encoding of the data for storage. Most types are stored in
@@ -91,7 +147,7 @@ type Encoding string
 
 func (e Encoding) String() string {
 	if e == "" {
-		return "<native>"
+		return "data:native"
 	}
 	return string(e)
 }
@@ -99,6 +155,12 @@ func (e Encoding) String() string {
 // Ext returns the encoding's standard file extension.
 func (e Encoding) Ext() string {
 	return string(e)
+}
+
+// Ok return true if the encoding is defined.
+func (e Encoding) Ok() bool {
+	_, ok := encodings[e]
+	return ok
 }
 
 // Class is the category of data that a type belongs to. JPG, PNG, GIF are all
@@ -109,8 +171,20 @@ func (c Class) String() string {
 	return string(c)
 }
 
+var types = map[Type]bool{
+	JPG: true,
+	PNG: true,
+	GIF: true,
+}
+
+var encodings = map[Encoding]bool{
+	Native: true,
+	Tar:    true,
+	GZip:   true,
+}
+
 var classOfType = map[Type]Class{
-	UnknownType: Uncategorized,
+	UnknownType: Unclassified,
 
 	// Image formats.
 	JPG: Image,
--- data/types_test.go
+++ data/types_test.go
@@ -10,13 +10,31 @@ func TestType(t *testing.T) {
 	tests := []struct {
 		desc      string
 		typ       Type
+		wantOk    bool
 		wantStr   string
 		wantExt   string
 		wantClass Class
 	}{
 		{
+			desc:      "Unknown",
+			typ:       UnknownType,
+			wantOk:    false,
+			wantStr:   "data:unknown",
+			wantExt:   "",
+			wantClass: Unclassified,
+		},
+		{
+			desc:      "other",
+			typ:       "foo",
+			wantOk:    false,
+			wantStr:   "foo",
+			wantExt:   "foo",
+			wantClass: Unclassified,
+		},
+		{
 			desc:      "jpg",
 			typ:       JPG,
+			wantOk:    true,
 			wantStr:   "jpg",
 			wantExt:   "jpg",
 			wantClass: Image,
@@ -24,6 +42,7 @@ func TestType(t *testing.T) {
 		{
 			desc:      "png",
 			typ:       PNG,
+			wantOk:    true,
 			wantStr:   "png",
 			wantExt:   "png",
 			wantClass: Image,
@@ -31,6 +50,7 @@ func TestType(t *testing.T) {
 		{
 			desc:      "gif",
 			typ:       GIF,
+			wantOk:    true,
 			wantStr:   "gif",
 			wantExt:   "gif",
 			wantClass: Image,
@@ -38,6 +58,9 @@ func TestType(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
+			if got, want := tt.typ.Ok(), tt.wantOk; got != want {
+				t.Errorf("Ok() got %t want %t", got, want)
+			}
 			if got, want := tt.typ.String(), tt.wantStr; got != want {
 				t.Errorf("String got %q want %q", got, want)
 			}
@@ -55,30 +78,44 @@ func TestEncoding(t *testing.T) {
 	tests := []struct {
 		desc    string
 		enc     Encoding
+		wantOk  bool
 		wantStr string
 		wantExt string
 	}{
 		{
 			desc:    "native",
 			enc:     Native,
-			wantStr: "<native>",
+			wantOk:  true,
+			wantStr: "data:native",
 			wantExt: "",
+		},
+		{
+			desc:    "undefined",
+			enc:     "foo",
+			wantOk:  false,
+			wantStr: "foo",
+			wantExt: "foo",
 		},
 		{
 			desc:    "tar",
 			enc:     Tar,
+			wantOk:  true,
 			wantStr: "tar",
 			wantExt: "tar",
 		},
 		{
 			desc:    "gzip",
 			enc:     GZip,
+			wantOk:  true,
 			wantStr: "gz",
 			wantExt: "gz",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
+			if got, want := tt.enc.Ok(), tt.wantOk; got != want {
+				t.Errorf("Ok() got %t want %t", got, want)
+			}
 			if got, want := tt.enc.String(), tt.wantStr; got != want {
 				t.Errorf("String got %q want %q", got, want)
 			}
@@ -93,37 +130,58 @@ func TestStored(t *testing.T) {
 	tests := []struct {
 		desc      string
 		stored    Stored
+		wantOk    bool
 		wantStr   string
 		wantExt   string
 		wantClass Class
 	}{
 		{
-			desc:    "native encoding",
-			stored:  Stored{JPG, Native},
+			desc:    "zero value",
+			stored:  Stored{},
+			wantOk:  false,
+			wantStr: "data:unknown",
+			wantExt: "",
+		},
+		{
+			desc:    "unknown type",
+			stored:  Stored{Encoding: GZip},
+			wantOk:  false,
+			wantStr: "gz",
+			wantExt: "gz",
+		},
+		{
+			desc:    "type with native encoding",
+			stored:  Stored{Type: JPG},
+			wantOk:  true,
 			wantStr: "jpg",
 			wantExt: "jpg",
 		},
 		{
-			desc:    "with encoding",
+			desc:    "type with encoding",
 			stored:  Stored{PNG, GZip},
+			wantOk:  true,
 			wantStr: "png.gz",
 			wantExt: "png.gz",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
+			if got, want := tt.stored.Ok(), tt.wantOk; got != want {
+				t.Errorf("Ok() got %t want %t", got, want)
+			}
 			if got, want := tt.stored.String(), tt.wantStr; got != want {
 				t.Errorf("String got %q want %q", got, want)
 			}
 			if got, want := tt.stored.Ext(), tt.wantExt; got != want {
 				t.Errorf("Ext got %q want %q", got, want)
 			}
-			got, err := ParseExt(tt.stored.String())
+			ext := tt.stored.Ext()
+			got, err := ParseExt(ext)
 			if err != nil {
 				t.Errorf("ParseExt round trip: %s", err)
 			}
 			if got != tt.stored {
-				t.Errorf("ParseExt got %s want %s", got, tt.stored)
+				t.Errorf("ParseExt got %v want %v", got, tt.stored)
 			}
 		})
 	}
@@ -148,16 +206,16 @@ func TestParseExt(t *testing.T) {
 			want: Stored{JPG, GZip},
 		},
 		{
-			// NOTE: being generous for now. could definitely reconsider this.
-			desc: "unknown type is allowed",
-			ext:  "foo.gz",
-			want: Stored{"foo", GZip},
+			desc:    "unknown type",
+			ext:     "foo.gz",
+			want:    Stored{"foo", GZip},
+			wantErr: errors.New("data: unknown type: \"foo\""),
 		},
 		{
-			// NOTE: being generous for now. could definitely reconsider this.
-			desc: "unknown encoding is allowed",
-			ext:  "jpg.foo",
-			want: Stored{JPG, "foo"},
+			desc:    "unknown encoding",
+			ext:     "jpg.foo",
+			want:    Stored{JPG, "foo"},
+			wantErr: errors.New("data: unknown encoding: \"foo\""),
 		},
 		{
 			desc:    "too many parts (will support this later)",
@@ -169,10 +227,10 @@ func TestParseExt(t *testing.T) {
 		t.Run(tt.desc, func(t *testing.T) {
 			got, err := ParseExt(tt.ext)
 			if !reflect.DeepEqual(err, tt.wantErr) {
-				t.Fatalf("ParseExt got err %q want %q", got, tt.wantErr)
+				t.Fatalf("ParseExt got err %q want %q", err, tt.wantErr)
 			}
 			if got != tt.want {
-				t.Fatalf("ParseExt got %q want %q", got, tt.want)
+				t.Fatalf("ParseExt got %v want %v", got, tt.want)
 			}
 		})
 	}
