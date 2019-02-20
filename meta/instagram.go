0f3cb8e284d738db16a576958af90da3db052584
Ryan Carver <ryan@ryancarver.com>
2019-02-19 17:54:48 -0800

    set json fields on Instagram meta

--- meta/instagram.go
+++ meta/instagram.go
@@ -7,58 +7,58 @@ import "time"
 type InstagramMedia struct {
 
 	// Media ID
-	ID string
+	ID string `json:"id"`
 
 	// If the media is part of a multi-media post like a carousel, this is
 	// the position of this individual piece of media.
-	Multiple bool
-	Position int
+	Multiple bool `json:"multiple,omitempty"`
+	Position int  `json:"position,omitempty"`
 
 	// Public URL
-	Link string
+	Link string `json:"link"`
 
 	// Basic information when posted.
-	User      InstagramUser
-	Location  InstagramLocation
-	Caption   string
-	CreatedAt time.Time
+	User      *InstagramUser     `json:"user,omitempty"`
+	Location  *InstagramLocation `json:"location,omitempty"`
+	Caption   string             `json:"caption,omitempty"`
+	CreatedAt time.Time          `json:"created_at"`
 
 	// Additional information when posted.
-	Filter      string
-	Tags        []string
-	TaggedUsers []InstagramTaggedUser
+	Filter      string                `json:"filter,omitempty"`
+	Tags        []string              `json:"tags,omitempty"`
+	TaggedUsers []InstagramTaggedUser `json:"tagged_users,omitempty"`
 
 	// User activity.
-	Likes    int
-	Comments []InstagramComment
+	Likes    int                `json:"likes"`
+	Comments []InstagramComment `json:"comments,omitempty"`
 }
 
 // InstagramUser describes a user on Instagram.
 type InstagramUser struct {
-	ID       string
-	Username string
-	FullName string
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	FullName string `json:"full_name,omitempty"`
 }
 
 // InstagramLocation describes geo location of an Instagram post.
 type InstagramLocation struct {
-	ID        string
-	Name      string
-	Latitude  float64
-	Longitude float64
+	ID        string  `json:"id"`
+	Name      string  `json:"name"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
 }
 
 // InstagramComment is a comment on an Instagram post.
 type InstagramComment struct {
-	ID   string
-	Text string
-	Date time.Time
-	From InstagramUser
+	ID       string    `json:"id"`
+	Text     string    `json:"text"`
+	Username string    `json:"username"`
+	Date     time.Time `json:"date"`
 }
 
 //InstagramTaggedUser is a user tagged in media.
 type InstagramTaggedUser struct {
-	Username string
-	X        float64
-	Y        float64
+	Username string  `json:"username"`
+	X        float64 `json:"x"`
+	Y        float64 `json:"y"`
 }
